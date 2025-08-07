b0d3961d055d081de3161326633de25c91ffab6a
DESKTOP-694GAFE\thdfp <thdfpdh@Naver.com>
2025-08-07 20:11:51 +0900

    modify

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // /api로 시작하는 요청을 http://localhost:3000으로 프록시
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
@@ -14,4 +13,5 @@ export default defineConfig({
       },
     },
   },
-})
+  // resolve.alias 옵션을 제거
+});
