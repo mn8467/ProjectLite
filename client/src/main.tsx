136b77f061273fbaa1975664a89038e93b895bfb
mn8467 <mn8467@naver.com>
2025-08-13 11:10:29 +0900

    boarddetail css complete

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -16,13 +16,14 @@ import Logout from './pages/Logout.tsx'
 import Mypage from './pages/Mypage.tsx';
 import Withdrawal from './pages/Withdrawal.tsx';
 import BoardDetail from './pages/BoardDetail.tsx'
+import MainLayout from './components/MainLayout.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/board/:boardId" element={<BoardDetail/>} />
+        <Route path="/board/:boardId" element={<MainLayout><BoardDetail/></MainLayout>} />
         <Route path="/app" element={<App />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
